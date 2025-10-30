d63b84f96dfc37ca111a8d629e9b126824e6c53e
svillors <tntzator9@gmail.com>
2025-10-30 21:05:15 +0100

    Send buses to frontend

--- server.py
+++ server.py
@@ -1,19 +1,43 @@
+import json
+from functools import partial
+
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
 
 
-async def echo_server(request):
+buses = {}
+
+
+async def server(request):
     ws = await request.accept()
     while True:
         try:
             message = await ws.get_message()
-            print(message)
+            bus_info = json.loads(message)
+            buses[bus_info['busId']] = bus_info
+        except ConnectionClosed:
+            break
+
+
+async def talk_to_browser(request):
+    ws = await request.accept()
+    while True:
+        try:
+            buses_info = list(buses.values())
+            payload = json.dumps({
+                "msgType": "Buses",
+                "buses": buses_info
+            })
+            await ws.send_message(payload)
+            await trio.sleep(1)
         except ConnectionClosed:
             break
 
 
 async def main():
-    await serve_websocket(echo_server, '127.0.0.1', 8080, ssl_context=None)
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(partial(serve_websocket, server, '127.0.0.1', 8080, ssl_context=None))
+        nursery.start_soon(partial(serve_websocket, talk_to_browser, '127.0.0.1', 8000, ssl_context=None))
 
 
 trio.run(main)
